8a7ca5b317404abef586c766f607848556affa12
Michael <mhooperjnr1995@hotmail.co.uk>
2020-10-14 20:48:08 +0100

    Started Flask pages with Bootstrap layout

--- flask_page.py
+++ flask_page.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello! This is your main page <h1>HOME <h1>"
+@app.route("/<name>/")
+def home(name):
+    return render_template("index.html", content = name)
+
+
+@app.route("/admin/")
+def admin():
+    return redirect(url_for("user", name = "Admin"))
+
+@app.route("/<name>/portfolio")
+def porfolio(name):
+    return f"Hello, {name}!"
 
 if __name__ == "__main__":
     app.run()
