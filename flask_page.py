348af56749d26232b15bf977d13c13cd710a65d2
Michael <mhooperjnr1995@hotmail.co.uk>
2020-10-16 23:49:51 +0100

    Added flash message

--- flask_page.py
+++ flask_page.py
@@ -1,8 +1,13 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+import os
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.permanent_session_lifetime = timedelta(days = 5)
 
-@app.route("/<name>/")
+
+@app.route("/home/<name>/")
 def home(name):
     return render_template("index.html", content = name)
 
@@ -14,6 +19,35 @@ def admin():
 @app.route("/<name>/portfolio")
 def porfolio(name):
     return f"Hello, {name}!"
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login.html"))
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been logged out, {user}", "info")
+    session.pop("user", None)
+
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run()
