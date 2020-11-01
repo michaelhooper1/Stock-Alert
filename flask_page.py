9296b46b8d043a8f57b0cf8729dce632bd2644f5
Michael <michaelhooper1995@outlook.com>
2020-11-01 02:44:12 +0000

    Fixed hashing login issue, plus started on portfolio watching

--- flask_page.py
+++ flask_page.py
@@ -1,9 +1,11 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-import bcrypt
 import os
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 from passlib.hash import sha256_crypt
+import sqlite3
+
+
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -13,6 +15,10 @@ app.permanent_session_lifetime = timedelta(days = 5)
 
 db = SQLAlchemy(app)
 
+user_tracking_company = db.Table("user_tracking_company",
+             db.Column(("user_id"), db.ForeignKey("user.id")),
+             db.Column(("company_id"), db.ForeignKey("company.id")))
+
 class User(db.Model):
     _id = db.Column("id", db.Integer, primary_key = True)
     name = db.Column(db.String(100))
@@ -20,11 +26,30 @@ class User(db.Model):
     email = db.Column(db.String(100))
     password = db.Column(db.Text, nullable = False)
 
+    tracks = db.relationship("Company", backref = "tracked_by", lazy = "dynamic", secondary = user_tracking_company)
+
     def __init__(self, name, username, email, password):
         self.name = name
         self.username = username
         self.email = email
         self.password = password
+
+
+class Company(db.Model):
+    _id = _id = db.Column("id", db.Integer, primary_key = True)
+    c_name = db.Column(db.String(200))
+    c_index = db.Column(db.String(5))
+    c_price = db.Column(db.Float)
+    
+    _tracked_by = db.relationship("User", secondary = user_tracking_company, backref = db.backref("user_tracking_company_backref", lazy="dynamic"))
+
+    def __init__(self, c_name, c_index):
+        self.c_name = c_name
+        self.c_index = c_index
+
+
+
+
 
 
 @app.route("/register", methods=["POST", "GET"])
@@ -47,7 +72,7 @@ def register():
         session["username"] = username
 
         preencrypted_password = request.form["password"]
-        password = sha256_crypt.encrypt(preencrypted_password)
+        password = sha256_crypt.hash(preencrypted_password)
         session["password"] = password
 
         confirmed_password = request.form["confirm"]
@@ -57,15 +82,17 @@ def register():
             return redirect(url_for("register"))
         
         if not username:
-            return "Missing Username", 400
+            flash("Please enter a username")
         if not email:
-            return "Missing email", 400
+            flash("Please enter an email")
         if not password:
-            return "Missing Password", 400
+            flash("Please enter a password")
 
         new_user = User(name = name, username = username, email = email, password= password)
         db.session.add(new_user)
         db.session.commit()
+        print(db)
+        print(new_user)
         flash("Welcome, {}".format(new_user.username))
         return redirect(url_for("user"))
       
@@ -79,31 +106,48 @@ def register():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        session.permanent = True
-        user = request.form["nm"]
-        password = request.form["password"]
-        session["user"] = user
+        with sqlite3.connect("users.sqlite3") as connection:
 
-        data = c.execute("SELECT * FROM users WHERE username = (%s)",
-                             thwart(request.form['username']))
+            session.permanent = True
+            user = request.form["nm"]
+            password = request.form["password"]
+            session["user"] = user
 
-        found_user = sha256_crypt.verify(password, data)
+            c = connection.cursor()
 
-        if found_user:
-            session["email"] = found_user.email
+            data = c.execute("SELECT * FROM user WHERE username = '{}'".format(user))
 
-        else:
-            flash("No such user exist, register to continue")
-            return redirect(url_for("register"))
+            data = c.fetchone()[4]
 
-        flash("Login successful")
-        return redirect(url_for("user"))
+            id_data = c.execute("SELECT id from user where username = '{}'".format(user))
+
+            id_data = c.fetchone()[0]
+
+            session["id"] = id_data
+       
+
+            found_user = sha256_crypt.verify(password, data)
+
+            
+
+            if found_user:
+                #session["email"] = found_user.email
+                return redirect(url_for("user"))
+
+            else:
+                flash("No such user exist, register to continue")
+                return redirect(url_for("register"))
+
     else:
         if "user" in session:
             flash("You're currently logged in")
             return redirect(url_for("user"))
 
-        return render_template("login.html")
+        
+
+    
+
+    return render_template("login.html")
 
 @app.route("/view")
 def view():
@@ -112,6 +156,7 @@ def view():
 @app.route("/")
 def home():
     if "user" in session:
+        user = session["name"]
         return redirect(url_for("user"))
     else:
         flash("Please log in to access your information")
@@ -122,14 +167,34 @@ def home():
 def admin():
     return redirect(url_for("user", name = "Admin"))
 
-@app.route("/<name>/portfolio")
-def porfolio(name):
-    return f"Hello, {name}!"
+@app.route("/portfolio")
+def porfolio():
+    
+    if "user" in session:
+       
 
+   
+        c = sqlite3.connect("users.sqlite3").cursor()
+        id_call = session["id"]
+        
+        tracking_data = c.execute("SELECT company_id FROM user_tracking_company WHERE user_id = '{}'".format(id_call))
+        
+        company_list = [x for x in tracking_data]
+        
+
+        if len(company_list) == 0:
+            flash("You are not tracking anything")
+
+        #Recall sqlAlchemy get() query is by primary key
+        user = User.query.get(id_call)
+    else:
+        return redirect(url_for("login"))
+
+    return render_template("portfolio.html", companies = user.tracks.all())
 
 @app.route("/user", methods=["POST", "GET"])
 def user():
-    email = None
+    
     if "user" in session:
         user = session["user"]
 
